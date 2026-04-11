261e604e4542f402c4fb99c9c278d466b4b1b94c
nadmax <maximilien.nadji@pm.me>
2026-04-11 19:44:21 +0200

    aaa

--- lib/list.c
+++ lib/list.c
@@ -63,11 +63,9 @@ void ListIterator_setval(ListIteratorClass* this, ...)
         raise("NULL value given");
 
     va_start(args, this);
-
     node = this->m_current;
     node->m_type = (Class *) va_arg(args, size_t);
     obj = va_new(node->m_type, &args);
-
     delete(node->m_value);
 
     node->m_value = obj;
@@ -99,6 +97,121 @@ static const ListIteratorClass _ListIteratorDescr = {
 };
 
 static const Class* ListIterator = (const Class*)&  _ListIteratorDescr;
+
+
+void Node_ctor(NodeClass* this, va_list* args)
+{
+    if (!this || !args)
+        raise("NULL value given");
+
+    this->m_type = va_arg(*args, Class*);
+    if (!this->m_type)
+        raise("NULL value given");
+
+    this->m_value = va_new(this->m_type, args);
+}
+
+void Node_dtor(NodeClass* this)
+{
+    if (!this)
+        raise("NULL value given");
+
+    delete(this->m_value);
+}
+
+char* Node_str(NodeClass* this)
+{
+    if (!this)
+        raise("Null value given");
+
+    return str(this->m_value);
+}
+
+void Node_set_prev(NodeClass* this, NodeClass* other)
+{
+    if (!this || !other)
+        raise("NULL value given");
+
+    this->m_prev = other;
+}
+
+void Node_set_next(NodeClass* this, NodeClass* other)
+{
+    if (!this || !other)
+        raise("NULL value given");
+
+    this->m_next = other;
+}
+
+void Node_set_value(NodeClass* this, Class *type, ...)
+{
+    va_list va;
+    Object *obj = NULL;
+
+    if (!this || !type)
+        raise("NULL value given");
+
+    va_start(va, type);
+    this->m_type = type;
+    obj = va_new(this->m_type, &va);
+    delete(this->m_value);
+
+    this->m_value = obj;
+    va_end(va);
+}
+
+NodeClass* Node_get_prev(NodeClass* this)
+{
+    if (!this)
+        raise("NULL value given");
+
+    return this->m_prev;
+}
+
+NodeClass* Node_get_next(NodeClass* this)
+{
+    if (!this)
+        raise("NULL value given");
+
+    return this->m_next;
+}
+
+Object *Node_get_value(NodeClass* this)
+{
+    if (!this)
+        raise("NULL value given");
+
+    return this->m_value;
+}
+
+static const NodeClass _NodeDescr = {
+    {
+        .__size__ = sizeof(NodeClass), 
+        .__name__ = "Node",
+        .__ctor__ = (ctor)&Node_ctor,
+        .__dtor__ = (dtor)&Node_dtor,
+        .__str__ = (to_string)&Node_str,
+        .__add__ = NULL,
+        .__sub__ = NULL,
+        .__mul__ = NULL,
+        .__div__ = NULL,
+        .__eq__ = NULL,
+        .__gt__ = NULL,
+        .__lt__ = NULL
+    },
+    .m_type = NULL,
+    .m_value = NULL,
+    .m_next = NULL,
+    .m_prev = NULL,
+    .__set_prev__ = (set_link)&Node_set_prev,
+    .__set_next__ = (set_link)&Node_set_next,
+    .__set_value__ = (set_value)&Node_set_value,
+    .__get_prev__ = (get_link)&Node_get_prev,
+    .__get_next__ = (get_link)&Node_get_next,
+    .__get_value__ = (node_get_value)&Node_get_value
+};
+
+static const Class* Node = (const Class*)& _NodeDescr;
 
 
 void List_ctor(ListClass* this, va_list* args)
@@ -153,7 +266,6 @@ void list_push_back(ListClass* this, ...)
     va_list va;
 
     va_start(va, this);
-
     if (!this)
         raise("Out of Memory");
 
@@ -193,6 +305,7 @@ void list_pop_front(ListClass* this)
 void list_pop_back(ListClass* this)
 {
     NodeClass* old_node = NULL;
+
     if (!this)
         raise("Out of Memory");
 
@@ -260,7 +373,6 @@ Object* List_getitem(ListClass* this, ...)
         raise("NULL value given");
 
     va_start(va, this);
-
     index = va_arg(va, size_t);
     if (index < this->m_size) {
         node = List_get_at(this, index);
@@ -284,7 +396,6 @@ void List_setitem(ListClass* this, ...)
         raise("NULL value given");
 
     va_start(va, this);
-
     pos = va_arg(va, size_t);
     if (pos >= this->m_size)
         raise("Index out of bounds");
@@ -310,7 +421,6 @@ char* List_str(ListClass* this)
 
     len += snprintf(NULL, 0, "<ListClass ()>");
     node = this->m_head;
-
     while (node) {
         if (!node->m_value)
             continue;
@@ -325,7 +435,6 @@ char* List_str(ListClass* this)
             free(str);
         } else
             len += snprintf(NULL, 0, "<%s>, ", obj_class->__name__);
-
         if (!node->m_next)
             len -= 2;
 
